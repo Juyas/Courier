0b6185306809c8059be058b9ea1f2ed898d677e7
Juyas <juyasetgames@gmail.com>
2026-03-13 10:44:58 +0100

    Add CourierPlugin as JavaPlugin main class along with the required package-info

---
+++ src/main/java/de/juyas/courier/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * The plugins main package.
+ */
+@DefaultQualifier(value = NotNull.class, locations = {FIELD, PARAMETER, RETURN})
+package de.juyas.courier;
+
+import org.checkerframework.framework.qual.DefaultQualifier;
+import org.jetbrains.annotations.NotNull;
+
+import static org.checkerframework.framework.qual.TypeUseLocation.FIELD;
+import static org.checkerframework.framework.qual.TypeUseLocation.PARAMETER;
+import static org.checkerframework.framework.qual.TypeUseLocation.RETURN;
